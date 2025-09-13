4b5d235163a23c3d3fc1d9ed53b5bc951ee5d79f
Kairi <kairimatsuda3@gmail.com>
2025-09-14 02:10:54 +0900

    fix: Change Flutter install location to user workspace

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # --- Flutter SDKのインストール ---
-git clone https://github.com/flutter/flutter.git --depth 1 /usr/local/flutter
-# Flutterコマンドにパスを通す
-echo 'export PATH="$PATH:/usr/local/flutter/bin"' >> ~/.bashrc
+# ユーザーが書き込み可能な /workspaces/ ディレクトリにインストール先を変更
+git clone https://github.com/flutter/flutter.git --depth 1 /workspaces/flutter
+
+# Flutterコマンドにパスを通す (新しいパスに変更)
+echo 'export PATH="$PATH:/workspaces/flutter/bin"' >> ~/.bashrc
 # すぐに使えるように現在のセッションでもパスを有効化
-export PATH="$PATH:/usr/local/flutter/bin"
+export PATH="$PATH:/workspaces/flutter/bin"
 
 # Flutterのセットアップ（Doctorで確認）
 flutter precache
 flutter doctor
 
 # --- Amplify CLIのインストール ---
-# Node.jsとnpmはベースイメージに含まれている
 npm install -g @aws-amplify/cli
 
 echo "✅ Post-create script finished successfully!"
