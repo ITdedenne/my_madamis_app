97a701c9a25b06e843d002ad96205c475d902ee1
ITdedenne <kairimatsuda3@gmail.com>
2025-09-28 15:28:52 +0000

    add

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_madamis_app/amplifyconfiguration.dart';
 // amplifyconfiguration.dartのimportは不要なので削除します
 import 'package:my_madamis_app/features/auth/presentation/notifiers/auth_state_notifier.dart';
 import 'package:my_madamis_app/features/auth/presentation/pages/login_page.dart';
@@ -20,40 +21,6 @@ Future<void> main() async {
 }
 
 Future<void> _configureAmplify() async {
-  // ---【重要】ステップ1でAWSコンソールからコピーした3つの値を、以下の変数に貼り付けてください ---
-  const userPoolId = 'ap-northeast-1_VlS5MtFSZ'; // 例: 'ap-northeast-1_XXXXXXXXX'
-  const userPoolWebClientId = '7k5v89dhlt9k3tkpbvnshn2gj3'; // 例: '5v7e06s334lhie3467gnnn6tce'
-  const region = 'ap-northeast-1'; // 例: 'ap-northeast-1'
-  // --------------------------------------------------------------------------------
-
-  // 認証に必要な最低限の情報だけで、手動で設定文字列を構築します
-  const amplifyconfig = '''{
-      "UserAgent": "aws-amplify-cli/2.0",
-      "Version": "1.0",
-      "auth": {
-          "plugins": {
-              "awsCognitoAuthPlugin": {
-                  "UserAgent": "aws-amplify-cli/0.1.0",
-                  "Version": "0.1.0",
-                  "IdentityManager": {
-                      "Default": {}
-                  },
-                  "CognitoUserPool": {
-                      "Default": {
-                          "PoolId": "$userPoolId",
-                          "AppClientId": "$userPoolWebClientId",
-                          "Region": "$region"
-                      }
-                  },
-                  "Auth": {
-                      "Default": {
-                          "authenticationFlowType": "USER_SRP_AUTH"
-                      }
-                  }
-              }
-          }
-      }
-  }''';
 
   try {
     if (Amplify.isConfigured) {
