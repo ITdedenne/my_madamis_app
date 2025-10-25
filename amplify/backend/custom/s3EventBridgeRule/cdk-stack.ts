48e0b46586c7633d54f5bc37771c6a454d82d883
ITdedenne <kairimatsuda3@gmail.com>
2025-10-25 07:11:06 +0000

    fix

--- amplify/backend/custom/s3EventBridgeRule/cdk-stack.ts
+++ amplify/backend/custom/s3EventBridgeRule/cdk-stack.ts
@@ -1,7 +1,7 @@
 // amplify/backend/custom/s3EventBridgeRule/cdk-stack.ts
 import * as cdk from 'aws-cdk-lib';
 import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
-import { Rule, EventPattern, RuleTargetInput } from 'aws-cdk-lib/aws-events';
+import { Rule, EventPattern } from 'aws-cdk-lib/aws-events';
 import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
 import { Function as Lambda } from 'aws-cdk-lib/aws-lambda';
 import { CfnPermission } from 'aws-cdk-lib/aws-lambda';
@@ -9,39 +9,54 @@ import { CfnPermission } from 'aws-cdk-lib/aws-lambda';
 export class cdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
     super(scope, id, props);
+
     /* Do not remove - Amplify CLI automatically injects the current deployment environment in this input parameter */
-    new cdk.CfnParameter(this, 'env', {
+    const env = new cdk.CfnParameter(this, 'env', {
       type: 'String',
       description: 'Current Amplify CLI env name',
     });
 
-    // --- リソース名の取得 ---
-    // Lambda関数名をAmplifyプロジェクト情報から構築
-    const functionName = AmplifyHelpers.getProjectInfo().projectName + 'csvImporterFunction' + '-' + AmplifyHelpers.getProjectInfo().envName;
-
-    // ★★★ 修正箇所: S3バケット名を CfnParameter から取得 ★★★
-    // Amplify CLI が 'amplify push' 時に自動的にこのパラメータに値を注入します。
-    // パラメータ名は amplify/backend/backend-config.json の custom リソースの dependsOn で指定した
-    // storage リソース名 (`myMadamisAppS3storage`) + 'BucketName' という命名規則になります。
-    const bucketNameParameter = new cdk.CfnParameter(this, 'storagemyMadamisAppS3storageBucketName', {
+    // --- 1. S3バケット名を取得 (Amplifyが渡すパラメータ) ---
+    // backend-config.json の dependsOn に基づき、Amplifyが自動で値を渡します
+    // category: 'storage', resourceName: 'myMadamisAppS3storage' (-> MyMadamisAppS3storage), attribute: 'BucketName'
+    // パラメータ名: storageMyMadamisAppS3storageBucketName
+    const bucketNameParameter = new cdk.CfnParameter(this, 'storageMyMadamisAppS3storageBucketName', {
         type: 'String',
-        description: 'Name of the S3 bucket created by Amplify Storage category'
+        description: 'Name of the S3 bucket (from dependsOn)'
     });
     const bucketName = bucketNameParameter.valueAsString;
-    // ----------------------------------------------------
 
-    // --- Lambda関数オブジェクトの取得 ---
-    const lambdaFunction = Lambda.fromFunctionName(this, 'csvImporterLambdaFunction', functionName);
+    // --- 2. Lambda関数のARNを取得 (Amplifyが渡すパラメータ) ---
+    // category: 'function', resourceName: 'csvImporterFunction' (-> CsvImporterFunction), attribute: 'Arn'
+    // パラメータ名: functionCsvImporterFunctionArn
+    const functionArnParameter = new cdk.CfnParameter(this, 'functionCsvImporterFunctionArn', {
+        type: 'String',
+        description: 'ARN of the csvImporterFunction (from dependsOn)'
+    });
+    const functionArn = functionArnParameter.valueAsString;
 
-    // --- EventBridge ルールの定義 ---
+    // --- 3. Lambda関数名を取得 (Amplifyが渡すパラメータ) ---
+    // CfnPermission で必要
+    // category: 'function', resourceName: 'csvImporterFunction' (-> CsvImporterFunction), attribute: 'Name'
+    // パラメータ名: functionCsvImporterFunctionName
+    const functionNameParameter = new cdk.CfnParameter(this, 'functionCsvImporterFunctionName', {
+        type: 'String',
+        description: 'Name of the csvImporterFunction (from dependsOn)'
+    });
+    const functionName = functionNameParameter.valueAsString;
+
+    // --- 4. ARNからLambda関数オブジェクトを取得 ---
+    const lambdaFunction = Lambda.fromFunctionArn(this, 'csvImporterLambdaFunction', functionArn);
+
+    // --- 5. EventBridge ルールの定義 ---
     const eventPattern: EventPattern = {
       source: ['aws.s3'],
-      detailType: ['Object Created'],
+      detailType: ['Object Created'], // オブジェクト作成イベント
       detail: {
         bucket: {
-          name: [bucketName] // パラメータから取得したバケット名を使用
+          name: [bucketName] // パラメータで受け取ったバケット名でフィルタ
         },
-        // 必要であればフィルタリングを追加
+        // 必要に応じてCSVファイルのみに絞り込む (コメントアウト中)
         // object: {
         //   key: [{ suffix: '.csv' }]
         // }
@@ -49,35 +64,30 @@ export class cdkStack extends cdk.Stack {
     };
 
     const rule = new Rule(this, 'S3CsvUploadRule', {
-      ruleName: `s3-${AmplifyHelpers.getProjectInfo().projectName}-csv-upload-rule-${AmplifyHelpers.getProjectInfo().envName}`, // プロジェクト名を含むように修正
-      description: 'Triggers Lambda when a CSV is uploaded to the specified S3 bucket',
+      // 環境名を含めてルール名を一意にする
+      ruleName: cdk.Fn.join('-', [
+        's3-csv-upload-rule',
+        AmplifyHelpers.getProjectInfo().projectName, // "mymadamisapp"
+        env.valueAsString // "dev" など
+      ]),
+      description: 'Triggers Lambda when a file is created in the S3 bucket',
       eventPattern: eventPattern,
     });
 
-    // --- Lambdaターゲットの追加 ---
+    // --- 6. Lambda関数をルールの一番目のターゲットとして追加 ---
     rule.addTarget(new LambdaFunction(lambdaFunction, {
-      // InputTransformer は前回同様コメントアウト (Lambda側でevent['detail']を処理するため)
-      /*
-      event: RuleTargetInput.fromObject({
-        bucket: JsonPath.stringAt('$.detail.bucket.name'),
-        key: JsonPath.stringAt('$.detail.object.key'),
-      })
-      */
+        // 必要に応じてリトライポリシーやデッドレターキュー(DLQ)を設定
+        // retryAttempts: 2,
     }));
-
-    // --- EventBridge が Lambda を呼び出すための権限を明示的に付与 ---
+    
+    // --- 7. EventBridge が Lambda を呼び出すための権限を付与 ---
+    // Lambda関数側にリソースベースポリシーが追加されます
     new CfnPermission(this, 'AllowEventBridgeInvokeLambdaPermission', {
-        functionName: lambdaFunction.functionName,
+        functionName: functionName, // ★ ARN ではなく Function *Name* を使用
         action: 'lambda:InvokeFunction',
         principal: 'events.amazonaws.com',
-        sourceArn: rule.ruleArn,
+        sourceArn: rule.ruleArn, // このルールからの呼び出しのみを許可
     });
-
-    // --- 不要になった可能性のあるパラメータ定義を削除 ---
-    // Amplifyが自動生成したかもしれない他のリソースパラメータ (例: GraphQL API IDなど) で、
-    // このスタック内で直接使用していないものは削除しても構いません。
-    // ただし、'env' と 'storagemyMadamisAppS3storageBucketName' は必要です。
-    // (functioncsvImporterFunctionArn なども不要です)
 
   }
 }
